d77ca4a407ed601b6c6834f40fe22b24699d5963
aimahusnain <aimahusnain@gmail.com>
2024-01-22 01:35:19 +0500

    jhguj

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,13 @@
+
+export const metadata = {
+  title: "Contact Me",
+  description: `Contact me through the form available on this page or email me at email`,
+};
+
+export default function Contact() {
+  return (
+    <div>
+      Contact Page
+    </div>
+  );
+}
